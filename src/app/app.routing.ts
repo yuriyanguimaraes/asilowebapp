d27104a6682c402dd24c638b679692e55dbfdb76
hppod <hppodgornik@gmail.com>
2020-01-22 22:54:56 -0300

    Routing file

---
+++ src/app/app.routing.ts
@@ -0,0 +1,13 @@
+import { Routes } from "@angular/router"
+
+export const ROUTES: Routes = [
+
+    // Rotas WebApp
+
+    // Rotas administrativas - children routes
+    //{path: 'admin', component:, children: [
+    //    {},
+    //    {}
+    //]}
+
+]
