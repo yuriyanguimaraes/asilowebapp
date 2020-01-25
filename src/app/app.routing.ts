1cc14f1876a94e1b831d6787b7b60dbae17aef6e
hppod <hppodgornik@gmail.com>
2020-01-25 17:45:46 -0300

    Remove app component for routes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,14 +1,11 @@
 import { Routes } from "@angular/router"
 
 //Import components
-import { AppComponent } from "./app.component"
 import { NoticiasComponent } from "./web-app/noticias/noticias.component"
 
 export const ROUTES: Routes = [
 
     // Rotas WebApp
-    { path: 'home', component: AppComponent },
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'noticias', component: NoticiasComponent }
 
     // Rotas administrativas - children routes
