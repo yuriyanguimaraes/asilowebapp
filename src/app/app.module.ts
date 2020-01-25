e99d5b59d5fd0b2a8957a9e1680e5246b1508d20
hppod <hppodgornik@gmail.com>
2020-01-25 17:37:52 -0300

    Implement Footer, adjust on header component and add router module for module app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, PreloadAllModules} from "@angular/router"
+
+//Routes
+import {ROUTES} from "./app.routing"
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './web-components/header/header.component';
@@ -12,7 +16,8 @@ import { FooterComponent } from './web-components/footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules})
   ],
   providers: [],
   bootstrap: [AppComponent]
