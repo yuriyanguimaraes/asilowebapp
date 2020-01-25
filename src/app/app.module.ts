76d7a28b31b002a82bd4592913a27b6e2e8811d4
hppod <hppodgornik@gmail.com>
2020-01-25 16:29:06 -0300

    Create components header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './web-components/header/header.component';
+import { FooterComponent } from './web-components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
