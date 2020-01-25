582d9fdb37417325b2127f113d6c00cd88293372
hppod <hppodgornik@gmail.com>
2020-01-25 17:43:10 -0300

    Adding some routes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,8 +1,15 @@
 import { Routes } from "@angular/router"
 
+//Import components
+import { AppComponent } from "./app.component"
+import { NoticiasComponent } from "./web-app/noticias/noticias.component"
+
 export const ROUTES: Routes = [
 
     // Rotas WebApp
+    { path: 'home', component: AppComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'noticias', component: NoticiasComponent }
 
     // Rotas administrativas - children routes
     //{path: 'admin', component:, children: [
