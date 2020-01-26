2aff9840844cf47081a18f8e33107dbad4c806ac
hppod <hppodgornik@gmail.com>
2020-01-26 19:26:52 -0300

    Adjust App Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {RouterModule, PreloadAllModules} from "@angular/router"
+import { RouterModule, PreloadAllModules } from "@angular/router"
+import { HttpModule } from "@angular/http"
+import { HttpClientModule } from "@angular/common/http"
 
 //Routes
-import {ROUTES} from "./app.routing"
+import { ROUTES } from "./app.routing"
 
+//Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './web-components/header/header.component';
 import { FooterComponent } from './web-components/footer/footer.component';
 import { NoticiasComponent } from './web-app/noticias/noticias.component';
+
+//Services
+import { NoticiasService } from "./web-app/services/noticias.service"
 
 @NgModule({
   declarations: [
@@ -19,9 +25,13 @@ import { NoticiasComponent } from './web-app/noticias/noticias.component';
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules})
+    HttpModule,
+    HttpClientModule,
+    RouterModule.forRoot(ROUTES, { preloadingStrategy: PreloadAllModules })
   ],
-  providers: [],
+  providers: [
+    NoticiasService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
