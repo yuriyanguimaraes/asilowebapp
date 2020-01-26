a6d4d75ecd0a1a03b9378ebab49d9c9b7f060cb3
hppod <hppodgornik@gmail.com>
2020-01-26 19:36:27 -0300

    Implement getNoticias method

--- src/app/web-app/services/noticias.service.ts
+++ src/app/web-app/services/noticias.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from "@angular/core"
 import { HttpClient } from "@angular/common/http"
 import { Observable } from "rxjs"
+import { AsiloWebApi } from "./../../app.api"
+import { Noticia } from "./../noticias/noticia.model"
 
 @Injectable()
 export class NoticiasService {
+
+    constructor(private http: HttpClient) { }
+
+    getNoticias(): Observable<Noticia[]> {
+        return this.http.get<Noticia[]>(`${AsiloWebApi}/noticia`)
+    }
 
 }
