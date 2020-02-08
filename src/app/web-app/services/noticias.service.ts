366460d9d0da572db77cd2bd93681e038ea06faf
hppod <hppodgornik@gmail.com>
2020-02-08 17:08:07 -0300

    Adding params

--- src/app/web-app/services/noticias.service.ts
+++ src/app/web-app/services/noticias.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core"
-import { HttpClient, HttpResponse } from "@angular/common/http"
+import { HttpClient, HttpResponse, HttpParams } from "@angular/common/http"
 import { Observable } from "rxjs"
 import { AsiloWebApi } from "./../../app.api"
 import { Noticia } from "./../noticias/noticia.model"
@@ -9,8 +9,10 @@ export class NoticiasService {
 
     constructor(private http: HttpClient) { }
 
+    params = new HttpParams()
+
     getNoticias(): Observable<HttpResponse<Noticia[]>> {
-        return this.http.get<Noticia[]>(`${AsiloWebApi}/noticia`, { observe: 'response' })
+        return this.http.get<Noticia[]>(`${AsiloWebApi}/noticia`, { params: this.params, observe: 'response' })
     }
 
 }
