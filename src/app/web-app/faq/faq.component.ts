fe34895bdbb44d44384330611e2d02e24dad48c6
yuriyanguimaraes <yuriyanguimaraes@gmail.com>
2020-02-09 21:25:03 -0200

    patch FAQ_0.02
    09/02/2020 -  Enviando conteudo adicionado!

--- src/app/web-app/faq/faq.component.ts
+++ src/app/web-app/faq/faq.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FAQService } from "./../services/faq.service"
+import { FAQ } from "./../faq/faq.model"
 
 @Component({
   selector: 'app-faq',
@@ -8,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FAQComponent implements OnInit {
 
-  constructor() { }
+  faq: FAQ[]
+
+  constructor(private faqs: FAQService) { }
 
   ngOnInit() {
+    this.getFAQ()
   }
 
+  getFAQ(){
+    this.faqs.getFAQ().subscribe(faq =>{
+      this.faq = faq
+    }) 
+  }
 }
