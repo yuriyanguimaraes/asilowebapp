89d878f727f9a42c16c710b32784c8b1d1039cbf
hppod <hppodgornik@gmail.com>
2020-02-11 21:23:38 -0300

    Remove environment variables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -76,7 +76,6 @@ import { DateAgoPipe } from "./web-app/pipes/date-ago.pipe";
     ReactiveFormsModule,
     RouterModule.forRoot(ROUTES, { preloadingStrategy: PreloadAllModules, onSameUrlNavigation: 'reload' }),
     AgmCoreModule.forRoot({ apiKey: 'AIzaSyAaqMUl8rdJBCXFuikA_nxaMIIfmq4Orx8' }),
-    // necessita proteger esta apiKey,via BD?
     BsDropdownModule.forRoot(),
     TooltipModule.forRoot(),
     ModalModule.forRoot(),
