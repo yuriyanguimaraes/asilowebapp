c30fe936852ca31bb36225e3f458a50f0450287b
SirLordX <andrewse2015@hotmail.com>
2020-02-13 16:32:11 -0300

    add ContatoService

---
+++ src/app/web-app/services/contato.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from "@angular/core"
+import { HttpClient, HttpResponse } from "@angular/common/http"
+import { Observable } from "rxjs"
+import { AsiloWebApi } from "./../../app.api"
+
+@Injectable()
+export class ContatoService {
+    
+}
