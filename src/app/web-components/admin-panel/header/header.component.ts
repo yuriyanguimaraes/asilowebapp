d8d944342fc82b7be214b3dcff1862c214e04757
hppod <hppodgornik@gmail.com>
2020-03-03 23:23:33 -0300

    Implement web components

--- src/app/web-components/admin-panel/header/header.component.ts
+++ src/app/web-components/admin-panel/header/header.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
+  selector: 'app-header-admin',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderAdminComponent implements OnInit {
 
   constructor() { }
 
