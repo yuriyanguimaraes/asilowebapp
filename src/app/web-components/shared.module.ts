cb6aa488e57a95b0b1467b7bca751c1494152865
hppod <hppodgornik@gmail.com>
2020-03-05 13:34:41 -0300

    Create shared module

---
+++ src/app/web-components/shared.module.ts
@@ -0,0 +1,17 @@
+//Import Modules
+import { NgModule } from "@angular/core"
+
+@NgModule({
+    declarations: [
+
+    ],
+    imports: [
+
+    ],
+    exports: [
+
+    ]
+})
+export class SharedModule {
+
+}
