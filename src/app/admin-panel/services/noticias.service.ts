a049e7a2203e24e80c37ec0b012019f85f09e41c
hppod <hppodgornik@gmail.com>
2020-03-08 21:15:51 -0300

    Npm install and move service

--- src/app/admin-panel/services/noticias.service.ts
+++ src/app/admin-panel/services/noticias.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Noticia } from '../web-app/noticias/noticia.model';
-import { AsiloWebApi } from '../app.api';
+import { Noticia } from '../../web-app/noticias/noticia.model';
+import { AsiloWebApi } from '../../app.api';
 
 @Injectable({
   providedIn: 'root'
