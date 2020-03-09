136d7325e30e32d3722afbf2876017c311ffd336
yuriyanguimaraes <yuriyanguimaraes@gmail.com>
2020-03-09 20:01:16 -0300

    agora vai

--- src/app/admin-panel/admin-panel.module.ts
+++ src/app/admin-panel/admin-panel.module.ts
@@ -3,17 +3,21 @@ import { NgModule } from "@angular/core"
 import { RouterModule } from "@angular/router"
 import { CommonModule } from "@angular/common"
 import { SharedModule } from "./../shared/shared.module"
+import { NgxPaginationModule } from 'ngx-pagination'
 
 //Import Components
 import { HeaderComponent } from "./../web-components/admin-panel/header/header.component"
 import { SidenavComponent } from "./../web-components/admin-panel/sidenav/sidenav.component"
 import { DashboardComponent } from "./dashboard/dashboard.component"
 
+
 //Import Routes
 import { AdminPanelRoutes } from "./admin-panel.routing"
+import { FaqAdmninComponent } from "./faq-admnin/faq-admnin.component"
 
 @NgModule({
     imports: [
+        NgxPaginationModule,
         CommonModule,
         SharedModule,
         RouterModule.forChild(AdminPanelRoutes)
@@ -21,7 +25,8 @@ import { AdminPanelRoutes } from "./admin-panel.routing"
     declarations: [
         HeaderComponent,
         SidenavComponent,
-        DashboardComponent
+        DashboardComponent,
+        FaqAdmninComponent
     ],
     exports: [
         HeaderComponent,
--- src/app/admin-panel/admin-panel.routing.ts
+++ src/app/admin-panel/admin-panel.routing.ts
@@ -1,13 +1,16 @@
 //Import Modules
 import { Routes } from "@angular/router"
 
+
 //Import Components
 import { DashboardComponent } from "./dashboard/dashboard.component"
+import { FaqAdmninComponent } from "./faq-admnin/faq-admnin.component"
 
 export const AdminPanelRoutes: Routes = [
     {
         path: 'admin', children: [
-            { path: '', component: DashboardComponent }
+            { path: '', component: DashboardComponent },
+            { path: 'faq', component: FaqAdmninComponent }
         ]
     }
 ]
