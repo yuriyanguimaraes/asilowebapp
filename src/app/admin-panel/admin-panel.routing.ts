56191e1ccf607e14bc633be178ef0a2ee0d1de81
hppod <hppodgornik@gmail.com>
2020-03-14 18:59:26 -0300

    Add route

--- src/app/admin-panel/admin-panel.routing.ts
+++ src/app/admin-panel/admin-panel.routing.ts
@@ -9,6 +9,7 @@ import { PortalTransparenciaComponent } from "./portal-transparencia/portal-tran
 import { SobreComponent } from "./sobre/sobre.component"
 import { NoticiasComponent } from "./noticias/noticias.component"
 import { ContatoAdminComponent } from "./contato/contato.component"
+import { NovoTransparenciaComponent } from "./portal-transparencia/novo-transparencia/novo-transparencia.component"
 
 export const AdminPanelRoutes: Routes = [
     {
@@ -16,7 +17,12 @@ export const AdminPanelRoutes: Routes = [
             { path: '', component: DashboardComponent },
             { path: 'faq', component: FaqAdmninComponent },
             { path: 'dashboard', component: DashboardComponent },
-            { path: 'transparencia', component: PortalTransparenciaComponent },
+            {
+                path: 'transparencia', children: [
+                    { path: '', component: PortalTransparenciaComponent },
+                    { path: 'novo', component: NovoTransparenciaComponent }
+                ]
+            },
             { path: 'sobre', component: SobreComponent },
             { path: 'noticias', component: NoticiasComponent },
             { path: 'contato', component: ContatoAdminComponent },
