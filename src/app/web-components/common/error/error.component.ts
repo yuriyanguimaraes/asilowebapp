263894f0a7e6a8d586a73c254ec8a6c9f8b8c22a
hppod <hppodgornik@gmail.com>
2020-03-14 09:05:18 -0300

    Implement error component

--- src/app/web-components/common/error/error.component.ts
+++ src/app/web-components/common/error/error.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LottieAnimationViewModule } from "ng-lottie"
 
 @Component({
   selector: 'app-error',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ErrorComponent implements OnInit {
 
-  constructor() { }
+  lottieConfig: any
+
+  constructor() {
+    LottieAnimationViewModule.forRoot()
+    this.lottieConfig = {
+      path: 'assets/animations/error.json',
+      autoplay: true,
+      loop: true
+    }
+  }
 
   ngOnInit() {
   }
