f3be703c2b33cd916f1cbb00aa77ececd021ea85
hppod <hppodgornik@gmail.com>
2020-03-14 19:14:16 -0300

    Implement input component

--- src/app/web-components/common/input/input.component.ts
+++ src/app/web-components/common/input/input.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, ContentChild, AfterContentInit } from '@angular/core';
+import { FormControlName } from "@angular/forms"
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css']
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements AfterContentInit {
+
+  @Input() label: string
+  @Input() errorMessage: string
+  @Input() showTip: boolean = true
+  @Input() value: string = null
+
+  input: any
+
+  @ContentChild(FormControlName) control: FormControlName
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterContentInit() {
+    this.input = this.control
+
+    if (this.input === undefined) {
+      throw new Error('Esse componente deve ser usado com uma diretiva FormControlName')
+    }
+  }
+
+  verifyInputEmpty(): boolean {
+    return this.input.value != "" ? true : false
+  }
+
+  hasSuccess(): boolean {
+    return this.input.valid && (this.input.dirty) && this.verifyInputEmpty()
+  }
+
+  hasError(): boolean {
+    return this.input.invalid && (this.input.dirty)
   }
 
 }
