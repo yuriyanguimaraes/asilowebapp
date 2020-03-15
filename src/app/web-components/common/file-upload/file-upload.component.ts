8c074effcd8a95520b82e985e708607385a608ed
hppod <hppodgornik@gmail.com>
2020-03-15 18:48:09 -0300

    Fix bug deploy

--- src/app/web-components/common/file-upload/file-upload.component.ts
+++ src/app/web-components/common/file-upload/file-upload.component.ts
@@ -17,7 +17,7 @@ export class FileUploadComponent implements ControlValueAccessor {
 
   @Input() progress
   onChange: Function
-  private file: File | null = null
+  public file: File | null = null
 
   @HostListener('change', ['$event.target.files']) emitFiles(event: FileList) {
     const file = event && event.item(0)
