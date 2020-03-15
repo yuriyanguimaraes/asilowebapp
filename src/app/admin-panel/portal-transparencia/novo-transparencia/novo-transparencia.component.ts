e7fe63985897b7041aad7380ee65b43aada8b97e
hppod <hppodgornik@gmail.com>
2020-03-15 00:29:14 -0300

    Implement upload files and post new documents

--- src/app/admin-panel/portal-transparencia/novo-transparencia/novo-transparencia.component.ts
+++ src/app/admin-panel/portal-transparencia/novo-transparencia/novo-transparencia.component.ts
@@ -3,6 +3,11 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms"
 import { BsModalService, BsModalRef } from "ngx-bootstrap/modal"
 import { ModalDialogComponent } from "./../../../web-components/common/modal-dialog/modal-dialog.component"
 import { TransparenciaService } from "./../../../shared/services/transparencia.service"
+import { Router } from "@angular/router"
+import { requiredFileType } from "./../../../shared/functions/upload-file-validator.function"
+import { uploadProgress } from "./../../../shared/functions/upload-progress.function"
+import { toResponseBody } from "./../../../shared/functions/to-response-body.function"
+import { toFormData } from "./../../../shared/functions/to-form-data.function"
 
 @Component({
   selector: 'app-novo-transparencia',
@@ -13,6 +18,8 @@ export class NovoTransparenciaComponent implements OnInit {
 
   modalRef: BsModalRef
   _formTransparencia: FormGroup
+  progress = 0
+  success = false
 
   selectOptionCategory: any[] = [
     { value: 'Documentos Oficiais' },
@@ -20,14 +27,14 @@ export class NovoTransparenciaComponent implements OnInit {
     { value: 'Relatório de Atividades' },
   ]
 
-  constructor(private _builder: FormBuilder, private _modal: BsModalService, private _service: TransparenciaService) { }
+  constructor(private _builder: FormBuilder, private _modal: BsModalService, private _service: TransparenciaService, private _router: Router) { }
 
   ngOnInit() {
     this._formTransparencia = this._builder.group({
       titulo: this._builder.control(null, [Validators.required]),
       descricao: this._builder.control(null, [Validators.required]),
       categoria: this._builder.control("Selecione", [Validators.required]),
-      arquivo: this._builder.control(null, [Validators.required])
+      arquivo: this._builder.control(null, [Validators.required, requiredFileType('pdf')])
     })
   }
 
@@ -42,6 +49,19 @@ export class NovoTransparenciaComponent implements OnInit {
       if (answer) {
         this._formTransparencia.reset()
       }
+    })
+  }
+
+  addNewDocuments() {
+    this.success = false
+    this._service.postDocuments(toFormData(this._formTransparencia.value)).pipe(
+      uploadProgress(progress => (this.progress = progress)),
+      toResponseBody()
+    ).subscribe(res => {
+      this.progress = 0
+      this.success = true
+      this._formTransparencia.reset()
+      this._router.navigate(['/admin/transparencia'])
     })
   }
 
---
+++ src/app/web-components/common/progress/progress.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-progress',
+  templateUrl: './progress.component.html',
+  styleUrls: ['./progress.component.css']
+})
+export class ProgressComponent {
+
+  @Input() progress = 0
+
+}
